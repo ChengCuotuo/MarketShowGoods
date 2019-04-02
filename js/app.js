64e9de0ef6c4c6ea33b967c3183f8214f17fbd35
ChengCuotuo <lei021191@outlook.com>
2019-04-02 19:43:50 +0800

    实现显示隐藏功能

--- js/app.js
+++ js/app.js
@@ -7,6 +7,35 @@
 $(function(){
     showhide();
     hoverSubMenu();
+    search();
+
+    /*
+     * 3.输入搜索关键字，列表显示匹配的结果
+     * */
+    function search(){
+        // focus 和 keyup时间，事件的内容是一致的，使用 on 可以同时监控两个事件，用空格隔开
+        $('#txtSearch').on('focus keyup', function(){
+            //如果输入框中有文本才显示列表
+            var txt = this.value.trim(); //获取去除两边空格之后的文本框中的文本
+            if(txt){
+                $('#search_helper').show();
+            }
+        }).blur(function(){
+            //隐藏列表
+            $('#search_helper').hide();
+        })
+    }
+
+    /*
+     * 2.鼠标移动切换二级当行菜单的切换显示和隐藏
+     * */
+    function hoverSubMenu(){
+        $('#category_items>div').hover(function(){
+            $(this).children(':last').show();
+        }, function(){
+            $(this).children(':last').hide();
+        });
+    }
 
     /*
     * 1.鼠标移入显示，移出隐藏
@@ -25,15 +54,6 @@ $(function(){
         })
     }
 
-    /*
-    * 2.鼠标移动切换二级当行菜单的切换显示和隐藏
-    * */
-    function hoverSubMenu(){
-        $('#category_items>div').hover(function(){
-            $(this).children(':last').show();
-        }, function(){
-            $(this).children(':last').hide();
-        });
-    }
+
 
 });
